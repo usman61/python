27cb3b9b5459d48f676afbfc172fd6e79969c29f
usman61 <usman.dcse.com>
2024-12-12 17:45:09 +0500

    updated demo file to run infinitely

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 # app.py
+import time
+
 # Append a string to a text file
 file_path = "output.txt"
 message = "Hello, World!"
@@ -8,3 +10,8 @@ with open(file_path, "a") as file:
     file.write(message + "\n")
 
 print(f"Message appended to {file_path}.")
+print("Keeping the container alive. Press Ctrl+C to stop.")
+
+# Keep the script running indefinitely
+while True:
+    time.sleep(60)  # Sleep for 60 seconds in each loop
